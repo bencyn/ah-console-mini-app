2d5788502cd8c766390b0d28ac1992bf70fa5b8a
bencyn <bensonnjung39@gmail.com>
2019-06-18 18:47:49 +0300

    story(frontend):view articles         -add view article by slug function

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def get(param):
     try:
         response = requests.get(base_url+param)
         response.raise_for_status()
-
         return response
 
     except Exception as error:
@@ -35,3 +34,15 @@ def list():
         click.echo(format(response.json()['articles']))
     except Exception:
         click.secho("List article response error", fg="red")
+
+
+@cli.command()
+@click.argument("slug")
+def article(slug):
+
+    url_format = "articles/{}".format(slug)
+    response = get(url_format)
+    try:
+        click.echo(format(response.json()['article']))
+    except Exception:
+        exit(1)

82840ece4a13e55e8a5a2a344121f4bd0f597346
bencyn <bensonnjung39@gmail.com>
2019-06-18 18:30:16 +0300

    story(frontend):list articles         -add list articles function

---
+++ app.py
@@ -0,0 +1,37 @@
+# cli.py
+import click
+import requests
+import pprint
+
+base_url = "https://ah-premier-staging.herokuapp.com/api/"
+
+
+def get(param):
+    try:
+        response = requests.get(base_url+param)
+        response.raise_for_status()
+
+        return response
+
+    except Exception as error:
+        click.secho(str(error), fg="red")
+
+
+def format(data):
+    pp = pprint.PrettyPrinter(indent=2)
+    return pp.pprint(data)
+
+
+@click.group()
+def cli():
+    print("==== Console AH Mini App =====")
+
+
+@cli.command()
+def list():
+    url_format = "articles/"
+    response = get(url_format)
+    try:
+        click.echo(format(response.json()['articles']))
+    except Exception:
+        click.secho("List article response error", fg="red")
